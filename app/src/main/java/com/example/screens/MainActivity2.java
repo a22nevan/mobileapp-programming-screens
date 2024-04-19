035d0c341db8e2a7b1e4d3c7f907100463350ecf
a22nevan <a22nevan@student.his.se>
2024-04-19 10:45:47 +0200

    -Added gif -Imported compatability for animating gifs in build.gradle -Added image and text to activity_main2.xml

--- app/src/main/java/com/example/screens/MainActivity2.java
+++ app/src/main/java/com/example/screens/MainActivity2.java
@@ -3,12 +3,22 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        imageView = findViewById(R.id.imageView);
+
+        Glide.with(this).load(R.drawable.maxwell).into(imageView);
+
     }
 }
